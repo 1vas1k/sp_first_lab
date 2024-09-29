4a8c849f298f44c448394e1ebc2e8be6f801f44a
1vas1k <ivan.lyoskin@gmail.com>
2024-09-29 16:03:46 +0300

    my own changes

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,11 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+
+// This is my own changes
+// Hello my name is Ivan
+// I am React Native developer
+// I really like programming
+// Hope I'll be strong developer
+// And this lab improve my git knowlage
